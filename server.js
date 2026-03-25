579e85e1bde7ad095e7930fe14bbf21fcb9ca15c
ganeshramila <ganeshramila1968@gmail.com>
2026-03-25 07:35:15 +0530

    initial commit

--- server.js
+++ server.js
@@ -14,7 +14,12 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 // Middlewares
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json());
 
 // Routes
